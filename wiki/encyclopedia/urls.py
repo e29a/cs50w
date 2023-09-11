13794bda3ecc3c83fbdcea274b95b727b3615579
e29a _ <alabiemmanuel029@gmail.com>
2023-09-11 08:49:01 +0000

    rm

--- wiki/encyclopedia/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<title>", views.wiki, name="wiki"),
-    path("search/", views.search, name="search")
-]
