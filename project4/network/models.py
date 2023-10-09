4168407bdd675766a80593716dbf48c15ed51c96
e29a <132907635+e29a@users.noreply.github.com>
2023-10-09 06:48:21 +0000

    ...

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=500)
+    author = models.CharField(max_length=64)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField()
+
+class Follow(models.Model):
+    follower = models.CharField(max_length=64)
+    following = models.CharField(max_length=64)
+
+class Like(models.Model):
+    post = models.IntegerField()
+    user = models.IntegerField()
--- project4/network/views.py
+++ project4/network/views.py
@@ -3,12 +3,29 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import User
+from .models import *
 
 
 def index(request):
-    return render(request, "network/index.html")
+    
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_num = request.GET.get('page', 1)
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    return render(request, "network/index.html", {
+        "posts": page_obj
+    })
 
 
 def login_view(request):
@@ -61,3 +78,102 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def new(request):
+    content = request.POST['content']
+    author = request.user.username
+    new_post = Post(content=content, author=author, likes=0)
+    new_post.save()
+    return HttpResponseRedirect('/')
+
+def profile(request, user):
+    user = User.objects.get(username=user)
+    posts = Post.objects.filter(author = request.user.username)
+    followers = Follow.objects.filter(following = request.user.username).count()
+    following = Follow.objects.filter(follower = request.user.username).count()
+
+    return render(request, "network/profile.html", {
+        "user" : user,
+        "posts" : posts,
+        "followers" : followers,
+        "following" : following
+    }) 
+
+def edit(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "GET":
+        return render(request, "network/edit.html", {
+            "post": post
+        })
+    else:
+        content = request.POST["content"]
+        post.content = content
+        post.save()
+        return HttpResponseRedirect("/")
+
+def following(request):
+    results = []
+    followings = Follow.objects.filter(follower = request.user.username)
+    posts = Follow.objects.all()
+
+    for post in posts:
+        for following in followings:
+            if post.author == following.following:
+                results.append[post]
+            else:
+                pass
+    
+    paginator = Paginator(results, 8)
+    page_num = request.GET.get('page', 1)
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    return render(request, "network/following.html", {
+        "posts": page_obj
+    })
+
+def follow(request, user):
+    follow = Follow(follower=request.user.username, following=user)
+    follow.save()
+    return HttpResponseRedirect("/")
+
+def unfollow(request, user):
+    unfollow = Follow.objects.get(follower=request.user.username, following=user)
+    unfollow.delete()
+    return HttpResponseRedirect("/")
+
+@login_required
+def like(request, id):
+    post = Post.objects.get(id=id)
+    post_likes = Like.objects.filter(post=post.id)
+    check = []
+
+    if post_likes:
+        for post_like in post_likes:
+            if post_like.user == request.user.id:
+                check.append(post_like)
+                break
+
+    if not check:
+        post.likes = post.likes + 1
+        post.save()
+        like = Like(user=request.user.id, post=id)
+        like.save()
+    else:
+        post.likes = post.likes - 1
+        post.save()
+        like = Like.objects.get(user=request.user.id, post=id)
+        like.delete()
+    
+    return JsonResponse(post.serialize())
+
+
+                
+    
+
+
